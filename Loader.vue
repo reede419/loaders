ea0fcd98f7a27e9030a10b8b97ff70d8d27d18ca
unknown <reede419@urk.net>
2022-06-01 15:01:19 +0300

    first commit

---
+++ Loader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="col-12 d-flex align-items-center h-100 p-5">
+    <div class="dot-opacity-loader">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loader',
+};
+</script>
+
+<style></style>
